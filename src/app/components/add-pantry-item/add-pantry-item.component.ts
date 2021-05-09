0be8dd8c93464e7661eb6dbb239f552d0010c721
Danny <danny@cultureiq.com>
2021-05-09 16:58:16 -0400

    prevent adding duplicate entry overwriting old one

--- src/app/components/add-pantry-item/add-pantry-item.component.ts
+++ src/app/components/add-pantry-item/add-pantry-item.component.ts
@@ -12,24 +12,32 @@ export class AddPantryItemComponent {
 	newItemName: string = null
 	adding = false
 	added = 0
+	errorMessage = ''
 	@Input() modalInstance: ModalController
 
-	closeModal() {
+	closeModal(): void {
 		this.modalInstance.dismiss({
 			added: this.added,
 		})
 	}
 
-	addElement() {
+	async addElement(): Promise<void> {
 		this.adding = true
 		const slug = this.storage.get_slug(this.newItemName)
-		this.storage.set(slug, {
+		const itemExists = await this.storage.get(slug)
+		if (itemExists) {
+			this.adding = false
+			this.errorMessage = `You already have ${this.newItemName} in your pantry`
+			return
+		}
+		await this.storage.set(slug, {
 			name: this.newItemName,
 			created: new Date(),
 			modified: new Date(),
 		})
 		this.adding = false
 		this.newItemName = ''
+		this.errorMessage = ''
 		this.added += 1
 	}
 }
