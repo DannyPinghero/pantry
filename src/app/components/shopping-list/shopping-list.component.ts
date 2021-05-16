8ba19fcf743105b3e38eb74585af3c77cfb78cc6
Danny <danny@cultureiq.com>
2021-05-16 15:33:01 -0400

    implement checkboxes and confirm purchase button

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit } from '@angular/core'
-import { PantryEntry } from '../../../types/pantry-types'
+import { Component } from '@angular/core'
+import { PantryEntry, AdHocShoppingEntry, PantryItem, AdHocShoppingItem } from '../../../types/pantry-types'
 import { StorageService } from '../../services/storage/storage.service'
 import { Observable } from 'rxjs'
-
-interface AdHocShoppingItem {
-	name: string
-	adhoc: true
-}
 
 @Component({
 	selector: 'app-shopping-list',
 	templateUrl: './shopping-list.component.html',
 	styleUrls: ['./shopping-list.component.scss'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
 	runningLow: PantryEntry[]
 	outOf: PantryEntry[]
-	otherItems: AdHocShoppingItem[]
-	inCart: (PantryEntry | AdHocShoppingItem)[] = []
+	inCart: (PantryEntry | AdHocShoppingEntry)[] = []
+	adHocItems: AdHocShoppingEntry[] = []
 
 	pantryList$: Observable<PantryEntry[]>
+
+	inCartCheckedStatusByKey: Record<string, boolean> = {}
 
 	constructor(public storage: StorageService) {}
 
@@ -35,21 +32,53 @@ export class ShoppingListComponent implements OnInit {
 			this.outOf = pantryList.filter(([, pantryItem]) => pantryItem.out && !pantryItem.inCart)
 			this.inCart = this.inCart.concat(pantryList.filter(([, pantryItem]) => pantryItem.inCart))
 		})
-	}
-
-	async ngOnInit(): Promise<void> {
-		await this.loadPantryList()
+		this.inCart.forEach(([key]) => {
+			this.inCartCheckedStatusByKey[key] = true
+		})
 	}
 
 	async ionViewDidEnter(): Promise<void> {
 		await this.loadPantryList()
 	}
 
-	placeInCart(entry: PantryEntry): void {
-		this.runningLow = this.runningLow.filter(([key]) => entry[0] !== key)
-		this.outOf = this.outOf.filter(([key]) => entry[0] !== key)
-		this.inCart.push(entry)
-		entry[1].inCart = true
-		this.storage.update(...entry)
+	async placeInCart([key, item]: PantryEntry | AdHocShoppingEntry): Promise<void> {
+		this.runningLow = this.runningLow.filter(([key_]) => key_ !== key)
+		this.outOf = this.outOf.filter(([key_]) => key_ !== key)
+		this.inCart.push([key, item])
+		item.inCart = true
+		this.inCartCheckedStatusByKey[key] = true
+		await this.storage.update(key, item)
+	}
+
+	async uncheckCartItem([key, item]: PantryEntry | AdHocShoppingEntry): Promise<void> {
+		this.inCartCheckedStatusByKey[key] = false
+		item.inCart = false
+
+		if (this.storage.isAdHoc(key)) {
+			this.adHocItems.push([key, item])
+		} else {
+			if ((item as PantryItem).runningLow) {
+				this.runningLow.push([key, item])
+			} else {
+				this.outOf.push([key, item])
+			}
+			item.inCart = false
+		}
+		this.inCart = this.inCart.filter(([key_]) => key_ != key)
+		await this.storage.update(key, item)
+	}
+
+	async emptyCheckedItemsFromCart(): Promise<void> {
+		for (const [key, item] of this.inCart.filter(([key]) => this.inCartCheckedStatusByKey[key])) {
+			if (this.storage.isAdHoc(key)) {
+				this.storage.delete(key)
+			} else {
+				;(item as PantryItem).runningLow = false
+				;(item as PantryItem).out = false
+				;(item as PantryItem).inCart = false
+				this.storage.update(key, item)
+			}
+		}
+		await this.loadPantryList()
 	}
 }
